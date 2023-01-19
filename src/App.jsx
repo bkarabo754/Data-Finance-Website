55f01309e14e2aedc374c22195851fb2563341b9
bkarabo754 <bkmasango26@gmail.com>
2023-01-19 11:38:59 +0200

    Analytics Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Analytics from './components/Analytics'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 
@@ -8,6 +9,7 @@ function App() {
     <div>
       <Navbar />
       <Hero />
+      <Analytics />
     </div>
   )
 }
---
+++ src/components/Analytics.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Laptop from '../assets/laptop.jpg'
+
+const Analytics = () => {
+  return (
+    <div className='w-full bg-white py-16 px-4'>
+      <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
+        <img src={Laptop} alt="lap" className='w-[500px] mx-auto my-4'/>
+        <div className='flex flex-col justify-center'>
+            <p className='text-[#00df9a] font-bold'>Data Analytics Dashboard</p>
+            <h1 className='md-text-4xl sm:text-3xl text-2xl font-bold py-2'>Manage Data Analytics Centrally</h1>
+            <p>It comprises the processes, tools and techniques of data analysis and management, including the      collection, organization, and storage of data. The chief aim of data analytics is to apply statistical analysis and technologies on data to find trends and solve problems.</p>
+            <button className='bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3 cursor-pointer hover:bg-[#00df9a] hover:text-black'>Getting Started</button>
+        </div>
+      </div>    
+    </div>
+  )
+}
+
+export default Analytics
