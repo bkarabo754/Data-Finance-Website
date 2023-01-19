9dd43d29c315f6e4c05cc064b42b8073ae03fb64
bkarabo754 <bkmasango26@gmail.com>
2023-01-19 10:56:24 +0200

    Hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,11 +12,11 @@ const Navbar = () => {
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
       <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
       <ul className='hidden md:flex'>
-        <li className='p-4'>Home</li>
-        <li className='p-4'>Company</li>
-        <li className='p-4'>Resources</li>
-        <li className='p-4'>About</li>
-        <li className='p-4'>Contact</li>
+        <li className='p-4 cursor-pointer hover:text-[#00df9a]'>Home</li>
+        <li className='p-4 cursor-pointer hover:text-[#00df9a]'>Company</li>
+        <li className='p-4 cursor-pointer hover:text-[#00df9a]'>Resources</li>
+        <li className='p-4 cursor-pointer hover:text-[#00df9a]'>About</li>
+        <li className='p-4 cursor-pointer hover:text-[#00df9a]'>Contact</li>
       </ul>
       <div onClick={handleNav} className='block md:hidden'>
           {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
