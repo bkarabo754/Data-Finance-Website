54504cff1e64bab4369b8ba337d0ce4cf5450b4d
bkarabo754 <bkmasango26@gmail.com>
2023-01-19 23:45:02 +0200

    Data Project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
 
   return (
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
-      <h1 className='w-full text-3xl font-bold text-[#00df9a]'>DATA.</h1>
+      <h1 className='w-full text-3xl font-bold text-[#00df9a] cursor-pointer'>DATA.</h1>
       <ul className='hidden md:flex'>
         <li className='p-4 cursor-pointer hover:text-[#00df9a]'>Home</li>
         <li className='p-4 cursor-pointer hover:text-[#00df9a]'>Company</li>
