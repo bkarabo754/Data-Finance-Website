410a22d495e46a8fe1f2cd316d4c4ac4dc07980b
bkarabo754 <bkmasango26@gmail.com>
2023-01-19 12:54:18 +0200

    Newsletter section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Analytics from './components/Analytics'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
+import Newsletter from './components/Newsletter'
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Hero />
       <Analytics />
+      <Newsletter />
     </div>
   )
 }
--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -5,7 +5,7 @@ const Analytics = () => {
   return (
     <div className='w-full bg-white py-16 px-4'>
       <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
-        <img src={Laptop} alt="lap" className='w-[500px] mx-auto my-4'/>
+        <img src={Laptop} alt="lap" className='w-[500px] mx-auto my-4 '/>
         <div className='flex flex-col justify-center'>
             <p className='text-[#00df9a] font-bold'>Data Analytics Dashboard</p>
             <h1 className='md-text-4xl sm:text-3xl text-2xl font-bold py-2'>Manage Data Analytics Centrally</h1>
